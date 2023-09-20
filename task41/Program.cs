1b06be149c7c98f9f58f090c6fd384d0d546402f
rbnhd6984 <ignorth@yandex.ru>
2023-09-20 22:00:52 +0300

    Домашнее задание к 24.09.23

---
+++ task41/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] myArray = UserInput();
+Console.WriteLine(string.Join(", ", myArray));
+int myCount = CountPositive(myArray);
+Console.Write($"Пользователь ввёл {myCount} числа больше нуля.");
+
+
+// Метод заполнения массива
+int[] UserInput() 
+{
+    Console.Write("Введите размер массива: ");
+    int arrSize = int.Parse(Console.ReadLine()!);
+    int[] array = new int[arrSize];
+    for (int i = 0; i < arrSize; i++)
+    {
+        Console.Write($"Введите число номер {i + 1}: ");
+        int temp = int.Parse(Console.ReadLine()!);
+        array[i] = temp;
+    }
+
+    return array;
+}
+
+// Метод подсчета числе больше нуля
+int CountPositive(int[] arr) 
+{
+    int count = 0;
+    foreach (var el in arr)
+    {
+        if (el > 0) count++;
+    }
+
+    return count;
+}
+
---
+++ task43/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double k1 = InputCoeff("Введите коэффициент k1: ");
+double k2 = InputCoeff("Введите коэффициент k2: ");
+double b1 = InputCoeff("Введите коэффициент b1: ");
+double b2 = InputCoeff("Введите коэффициент b2: ");
+
+LineIntersection();
+
+// Функция ввода коэффициента
+double InputCoeff(string msg)
+{
+    Console.Write(msg);
+    double num = double.Parse(Console.ReadLine()!);
+    return num;
+}
+
+// Функция нахождения точки пересечения двух прямых
+void LineIntersection()
+{
+    if (k1 == k2)
+    {
+        // Если прямые параллельны, то выводим сообщение.
+        Console.WriteLine("Прямые параллельны, точка пересечения не существует.");
+    }
+    else
+    {
+        // Находим точку пересечения.   
+        double x = (b2 - b1) / (k1 - k2), y = k1 * x + b1;
+        Console.WriteLine($"Координаты точки пересечения {x}, {y}");
+    }
+}
+
