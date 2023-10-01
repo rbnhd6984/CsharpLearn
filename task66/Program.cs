50d7cd716920f887a81a4a2a6249a56e6fd0138e
rbnhd6984 <ignorth@yandex.ru>
2023-10-01 17:28:52 +0300

    Промежуточная аттестация к 07.10.23

---
+++ task66/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write(AmountRangeNumbers(4, 15));
+
+int AmountRangeNumbers(int M, int N)
+{
+    if (M == N + 1) return 0;
+    else return M + AmountRangeNumbers(M + 1, N);
+}
